ca3c7c41c00dfb598eb974347451338688666bbc
Spencer B Liberto <spencerbliberto@gmail.com>
2013-09-21 14:22:03 -0400

    Implementing processArticle

---
+++ src/com/media_moderator/article_data/IndividualKeyword.java
@@ -0,0 +1,16 @@
+package com.media_moderator.article_data;
+
+public class IndividualKeyword {
+	String word;
+	double relevance;
+	String sentiment;
+	
+	public IndividualKeyword(String word, double relevance, String sentiment) {
+		super();
+		this.word = word;
+		this.relevance = relevance;
+		this.sentiment = sentiment;
+	}
+	
+
+}
