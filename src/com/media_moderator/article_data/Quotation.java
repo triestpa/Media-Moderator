c36b76c71b146ee7513f8bbff2210196167f0e58
Spencer B Liberto <spencerbliberto@gmail.com>
2013-09-21 14:23:28 -0400

    Implementing processArticle

---
+++ src/com/media_moderator/article_data/Quotation.java
@@ -0,0 +1,13 @@
+package com.media_moderator.article_data;
+
+public class Quotation {
+	String text;
+	String sentiment;
+	
+	public Quotation(String text, String sentiment) {
+		super();
+		this.text = text;
+		this.sentiment = sentiment;
+	}
+		
+}
